a9f1db45c26542aeb2c673f17953a426d5b1b1bc
Sophie <65089742+sophie-72@users.noreply.github.com>
2025-06-29 14:40:01 -0400

    print program instructions

---
+++ binary_analysis.py
@@ -0,0 +1,30 @@
+import sys
+
+from elftools.elf.descriptions import describe_reloc_type
+from elftools.elf.elffile import ELFFile
+from capstone import *
+from elftools.elf.relocation import RelocationSection
+
+
+def process_file(filename):
+    with open(filename, 'rb') as f:
+        elffile = ELFFile(f)
+
+        code = elffile.get_section_by_name('.text')
+        opcodes = code.data()
+        addr = code['sh_addr']
+
+        print(f"Entry Point: {hex(elffile.header['e_entry'])}")
+
+        md = Cs(CS_ARCH_X86, CS_MODE_64)
+
+        instructions = []
+        for i in md.disasm(opcodes, addr):
+            instructions.append(i)
+
+        for i in instructions:
+            print("0x%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        process_file(sys.argv[1])
