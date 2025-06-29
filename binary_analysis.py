84c58b0f1f618e47edd836c102207943e885c371
Sophie <65089742+sophie-72@users.noreply.github.com>
2025-06-29 18:06:31 -0400

    start translation to pseudocode

--- binary_analysis.py
+++ binary_analysis.py
@@ -4,7 +4,7 @@ from capstone import *
 from elftools.elf.elffile import ELFFile
 
 
-def process_file(filename):
+def get_file_instructions(filename):
     with open(filename, 'rb') as f:
         elffile = ELFFile(f)
 
@@ -12,17 +12,86 @@ def process_file(filename):
         opcodes = code.data()
         addr = code['sh_addr']
 
-        print(f"Entry Point: {hex(elffile.header['e_entry'])}")
-
         md = Cs(CS_ARCH_X86, CS_MODE_64)
 
         instructions = []
         for i in md.disasm(opcodes, addr):
             instructions.append(i)
 
+        return instructions
+
+def get_pseudocode(instructions):
+        pseudocode = []
         for i in instructions:
+            addr = i.address
+            mnemonic = i.mnemonic
+            op_str = i.op_str
+
+            if mnemonic == 'mov':
+                line = f"{op_str.split(',')[0].strip()} = {op_str.split(',')[1].strip()}"
+            elif mnemonic == 'add':
+                line = f"{op_str.split(',')[0].strip()} += {op_str.split(',')[1].strip()}"
+            elif mnemonic == 'sub':
+                line = f"{op_str.split(',')[0].strip()} -= {op_str.split(',')[1].strip()}"
+
+            elif mnemonic == 'xor':
+                line = f"{op_str.split(',')[0].strip()} ^= {op_str.split(',')[1].strip()}"
+
+            elif mnemonic == 'and':
+                line = f"{op_str.split(',')[0].strip()} &= {op_str.split(',')[1].strip()}"
+
+            elif mnemonic == 'or':
+                line = f"{op_str.split(',')[0].strip()} |= {op_str.split(',')[1].strip()}"
+
+            elif mnemonic == 'lea':
+                dest, src = op_str.split(', ')
+                line = f"{dest.strip()} = address_of({src.strip()})"
+
+            elif mnemonic == 'call':
+                line = f"call {op_str.strip()}"
+
+            elif mnemonic == 'ret':
+                line = "return"
+
+            elif mnemonic == 'cmp':
+                line = f"compare {op_str.strip()}"
+
+            elif mnemonic == 'je':
+                line = f"if (condition) goto {op_str.strip()}"
+
+            elif mnemonic == 'jne':
+                line = f"if (!condition) goto {op_str.strip()}"
+
+            elif mnemonic == 'jmp':
+                line = f"goto {op_str.strip()}"
+
+            elif mnemonic == 'test':
+                line = f"test {op_str.strip()}"
+
+            elif mnemonic == 'nop':
+                line = "no operation"
+
+            elif mnemonic == 'hlt':
+                line = "halt"
+
+            elif mnemonic == 'endbr64':
+                line = "end branch"
+            elif mnemonic == 'push':
+                line = f"stack.push({op_str.strip()})"
+            elif mnemonic == 'pop':
+                line = f"{op_str.strip()} = stack.pop()"
+            else:
+                line = f"{mnemonic}"
+
             print("0x%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))
+            print(f"0x{addr:x}:\t{line}\n")
+            pseudocode.append(line)
+        return pseudocode
+
+def main():
+    instructions = get_file_instructions(sys.argv[1])
+    get_pseudocode(instructions)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
-        process_file(sys.argv[1])
+        main()
