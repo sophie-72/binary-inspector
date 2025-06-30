064a3c41c0fc0f3dd9e2283a9a4bca8dd6aad663
Sophie <65089742+sophie-72@users.noreply.github.com>
2025-06-29 21:44:06 -0400

    format code with black

--- binary_analysis.py
+++ binary_analysis.py
@@ -6,12 +6,12 @@ from elftools.elf.elffile import ELFFile
 
 
 def get_file_instructions(filename):
-    with open(filename, 'rb') as f:
+    with open(filename, "rb") as f:
         elffile = ELFFile(f)
 
-        code = elffile.get_section_by_name('.text')
+        code = elffile.get_section_by_name(".text")
         opcodes = code.data()
-        addr = code['sh_addr']
+        addr = code["sh_addr"]
 
         md = Cs(CS_ARCH_X86, CS_MODE_64)
 
@@ -23,24 +23,23 @@ def get_file_instructions(filename):
 
 
 def translate_instructions(instructions):
-        translated_instructions = []
-        for i in instructions:
-            addr = i.address
-            mnemonic = i.mnemonic
-            op_str = i.op_str
+    translated_instructions = []
+    for i in instructions:
+        mnemonic = i.mnemonic
+        op_str = i.op_str
 
-            if "rip" in op_str:
-                next_instruction = instructions[instructions.index(i) + 1]
-                next_instruction_addr = next_instruction.address
-                op_str = op_str.replace("rip", hex(next_instruction_addr))
+        if "rip" in op_str:
+            next_instruction = instructions[instructions.index(i) + 1]
+            next_instruction_addr = next_instruction.address
+            op_str = op_str.replace("rip", hex(next_instruction_addr))
 
-            hex_addition = re.search("0x[0-9a-f]+\\s\\+\\s0x[0-9a-f]+", op_str)
-            if hex_addition:
-                result = eval(hex_addition.group())
-                op_str = op_str.replace(hex_addition.group(), hex(result))
+        hex_addition = re.search("0x[0-9a-f]+\\s\\+\\s0x[0-9a-f]+", op_str)
+        if hex_addition:
+            result = eval(hex_addition.group())
+            op_str = op_str.replace(hex_addition.group(), hex(result))
 
-            if "qword ptr [" in op_str:
-                op_str = op_str.replace("qword ptr [","memory[")
+        if "qword ptr [" in op_str:
+            op_str = op_str.replace("qword ptr [", "memory[")
 
             if mnemonic == 'mov':
                 line = f"{op_str.split(',')[0].strip()} = {op_str.split(',')[1].strip()}"
@@ -49,76 +48,83 @@ def translate_instructions(instructions):
             elif mnemonic == 'sub':
                 line = f"{op_str.split(',')[0].strip()} -= {op_str.split(',')[1].strip()}"
 
-            elif mnemonic == 'xor':
-                left = op_str.split(',')[0].strip()
-                right = op_str.split(',')[1].strip()
+        elif mnemonic == "xor":
+            left = op_str.split(",")[0].strip()
+            right = op_str.split(",")[1].strip()
 
-                if left == right:
-                    line = f"{left} = 0"
-                else:
-                    line = f"{left} ^= {right}"
-
-            elif mnemonic == 'and':
-                line = f"{op_str.split(',')[0].strip()} &= {op_str.split(',')[1].strip()}"
-
-            elif mnemonic == 'or':
-                line = f"{op_str.split(',')[0].strip()} |= {op_str.split(',')[1].strip()}"
-
-            elif mnemonic == 'lea':
-                dest, src = op_str.split(', ')
-                line = f"{dest.strip()} = {src.strip()[1:-1]}"
-
-            elif mnemonic == 'call':
-                line = f"call {op_str.strip()}"
-
-            elif mnemonic == 'ret':
-                line = "return"
-
-            elif mnemonic == 'cmp':
-                line = f"compare {op_str.strip()}"
-
-            elif mnemonic == 'je':
-                line = f"if (condition) goto {op_str.strip()}"
-
-            elif mnemonic == 'jne':
-                line = f"if (!condition) goto {op_str.strip()}"
-
-            elif mnemonic == 'jmp':
-                line = f"goto {op_str.strip()}"
-
-            elif mnemonic == 'test':
-                line = f"test {op_str.strip()}"
-
-            elif mnemonic == 'nop':
-                line = "no operation"
-
-            elif mnemonic == 'hlt':
-                line = "halt"
-
-            elif mnemonic == 'endbr64':
-                line = "end branch"
-            elif mnemonic == 'push':
-                line = f"stack.push({op_str.strip()})"
-            elif mnemonic == 'pop':
-                line = f"{op_str.strip()} = stack.pop()"
+            if left == right:
+                line = f"{left} = 0"
             else:
-                line = f"{mnemonic}"
+                line = f"{left} ^= {right}"
 
-            translated_instructions.append(line)
-        return translated_instructions
+        elif mnemonic == "and":
+            line = f"{op_str.split(',')[0].strip()} &= {op_str.split(',')[1].strip()}"
+
+        elif mnemonic == "or":
+            line = f"{op_str.split(',')[0].strip()} |= {op_str.split(',')[1].strip()}"
+
+        elif mnemonic == "lea":
+            dest, src = op_str.split(", ")
+            line = f"{dest.strip()} = {src.strip()[1:-1]}"
+
+        elif mnemonic == "call":
+            line = f"call {op_str.strip()}"
+
+        elif mnemonic == "ret":
+            line = "return"
+
+        elif mnemonic == "cmp":
+            line = f"compare {op_str.strip()}"
+
+        elif mnemonic == "je":
+            line = f"if (condition) goto {op_str.strip()}"
+
+        elif mnemonic == "jne":
+            line = f"if (!condition) goto {op_str.strip()}"
+
+        elif mnemonic == "jmp":
+            line = f"goto {op_str.strip()}"
+
+        elif mnemonic == "test":
+            line = f"test {op_str.strip()}"
+
+        elif mnemonic == "nop":
+            line = "no operation"
+
+        elif mnemonic == "hlt":
+            line = "halt"
+
+        elif mnemonic == "endbr64":
+            line = "end branch"
+        elif mnemonic == "push":
+            line = f"stack.push({op_str.strip()})"
+        elif mnemonic == "pop":
+            line = f"{op_str.strip()} = stack.pop()"
+        else:
+            line = f"{mnemonic}"
+
+        translated_instructions.append(line)
+    return translated_instructions
+
 
 def write_to_file(executable_name, instructions, translated_instructions):
     filename = f"{executable_name}.asm"
 
-    with open(filename, 'w') as file:
-        for (instruction, translated_instruction) in zip(instructions, translated_instructions):
-            file.write(f"0x{instruction.address:x}:\t{instruction.mnemonic}\t{instruction.op_str}\t; {translated_instruction}\n")
+    with open(filename, "w") as file:
+        for instruction, translated_instruction in zip(
+            instructions, translated_instructions
+        ):
+            file.write(
+                f"0x{instruction.address:x}:\t{instruction.mnemonic}\t{instruction.op_str}\t; {translated_instruction}\n"
+            )
+
 
 def main():
     instructions = get_file_instructions(sys.argv[1])
-    translated_instructions =  translate_instructions(instructions)
+    translated_instructions = translate_instructions(instructions)
     write_to_file(sys.argv[1], instructions, translated_instructions)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) == 2:
         main()
