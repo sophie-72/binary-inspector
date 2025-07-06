278e61e8053eb3ebb6248b74ea9a1391e2f4ef5d
Sophie <65089742+sophie-72@users.noreply.github.com>
2025-07-06 15:24:16 -0400

    add docstring in output.py

--- src/output.py
+++ src/output.py
@@ -1,3 +1,5 @@
+"""Write program output to a file."""
+
 from typing import List, Dict
 
 from models import Instruction
@@ -6,12 +8,20 @@ from models import Instruction
 def write_to_file(
     executable_name: str, instructions: Dict[str, List[Instruction]]
 ) -> None:
+    """
+    Write assembly instructions and translations to a file.
+    :param executable_name: ELF file name
+    :param instructions: dictionary of instructions by section name
+    """
     filename = f"{executable_name}.asm"
 
-    with open(filename, "w") as file:
+    with open(filename, "w", encoding="utf-8") as file:
         for section_name, section_instructions in instructions.items():
             if section_name == ".text":
                 for instruction in section_instructions:
                     file.write(
-                        f"0x{instruction.address:x}:\t{instruction.mnemonic}\t{instruction.op_str}\t; {instruction.translation}\n"
+                        f"0x{instruction.address:x}:\t"
+                        f"{instruction.mnemonic}\t"
+                        f"{instruction.op_str}\t"
+                        f"; {instruction.translation}\n"
                     )
