5d3aca271857eeead1deb705cadd67659dd51fda
Sophie <65089742+sophie-72@users.noreply.github.com>
2025-07-06 15:18:03 -0400

    add docstring in main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+"""Entry point of the binary-inspector project."""
+
 import sys
 
 from control_flow_graph import (
@@ -12,16 +14,15 @@ from output import write_to_file
 from translation import translate_instructions
 
 
-def main():
-    instructions = get_file_instructions(executable)
-    relocations = get_file_relocations(executable)
-    strings = get_file_strings(executable)
-    translate_instructions(instructions, relocations, strings)
-    write_to_file(executable, instructions)
-    print_main_function_graph(instructions, executable)
-
-
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         executable = sys.argv[1]
-        main()
+        instructions = get_file_instructions(executable)
+        relocations = get_file_relocations(executable)
+        strings = get_file_strings(executable)
+        translate_instructions(instructions, relocations, strings)
+        write_to_file(executable, instructions)
+        print_main_function_graph(instructions, executable)
+    else:
+        print("Usage: main.py <executable>")
+        sys.exit(1)
