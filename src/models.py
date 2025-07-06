90c693618a0472b6de7c00cb56047066cc5ee44f
Sophie <65089742+sophie-72@users.noreply.github.com>
2025-07-06 13:47:40 -0400

    fix typing warnings in models.py

--- src/models.py
+++ src/models.py
@@ -2,23 +2,25 @@ from typing import Optional, List
 
 
 class Instruction:
-    def __init__(self, address, mnemonic, op_str) -> None:
-        self.address: int = address
-        self.mnemonic: str = mnemonic
-        self.op_str: str = op_str
+    def __init__(self, address: int, mnemonic: str, op_str: str) -> None:
+        self.address = address
+        self.mnemonic = mnemonic
+        self.op_str = op_str
         self.translation: Optional[str] = None
 
 
 class BasicBlock:
-    def __init__(self, start_address, instructions):
-        self.start_address: int = start_address
+    def __init__(self, start_address: int, instructions: List[Instruction]) -> None:
+        self.start_address = start_address
         self.end_address = instructions[-1].address if instructions else start_address
-        self.instructions: List[Instruction] = instructions
+        self.instructions = instructions
 
 
 class Function:
-    def __init__(self, name, start_address, instructions):
-        self.name: str = name
-        self.start_address: int = start_address
-        self.instructions: List[Instruction] = instructions
+    def __init__(
+        self, name: str, start_address: int, instructions: List[Instruction]
+    ) -> None:
+        self.name = name
+        self.start_address = start_address
+        self.instructions = instructions
         self.basic_blocks: List[BasicBlock] = []
