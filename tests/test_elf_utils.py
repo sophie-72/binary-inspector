c03e7aca2adcef116525d0941fbc3809d49f5442
Sophie <65089742+sophie-72@users.noreply.github.com>
2025-07-10 21:12:23 -0400

    add a test for get_file_relocations

--- tests/test_elf_utils.py
+++ tests/test_elf_utils.py
@@ -1,15 +1,13 @@
 import unittest
 from unittest.mock import patch, MagicMock
 
-from src.elf_utils import get_file_instructions
+from src.elf_utils import get_file_instructions, get_file_relocations
 from src.models import Instruction
 
 
-class TestGetFileInstructions(unittest.TestCase):
+class TestElfUtils(unittest.TestCase):
     @patch("src.elf_utils._open_elf_file")
-    def test_given_filename_with_opcodes_when_getting_file_instructions_then_return_instructions(
-        self, mock_open_elf_file
-    ):
+    def test_get_file_instructions(self, mock_open_elf_file):
         mock_elf_file = MagicMock()
         mock_section = MagicMock()
         mock_section.name = ".text"
@@ -30,3 +28,29 @@ class TestGetFileInstructions(unittest.TestCase):
 
         result = get_file_instructions("dummy.elf")
         self.assertEqual(result, expected_instructions)
+
+    @patch("src.elf_utils._open_elf_file")
+    def test_get_file_relocations(self, mock_open_elf_file):
+        mock_elf_file = MagicMock()
+        mock_rela_dyn = MagicMock()
+        mock_rela_dyn.__getitem__.side_effect = lambda key: {"sh_link": 0}[key]
+        mock_relocation = MagicMock()
+        mock_relocation.__getitem__.side_effect = lambda key: {
+            "r_info_sym": 0,
+            "r_offset": 0x1000,
+        }[key]
+        mock_rela_dyn.iter_relocations.return_value = [mock_relocation]
+        mock_symbol = MagicMock()
+        mock_symbol.name = "my_function"
+        mock_symbol_table = MagicMock()
+        mock_symbol_table.get_symbol.return_value = mock_symbol
+        mock_elf_file.get_section_by_name.return_value = mock_rela_dyn
+        mock_elf_file.get_section.return_value = mock_symbol_table
+        mock_open_elf_file.return_value = mock_elf_file
+
+        expected_relocations = {
+            "0x1000": "my_function",
+        }
+
+        result = get_file_relocations("dummy.elf")
+        self.assertEqual(result, expected_relocations)
