34ddac5d6f04d3db2194574644f301a29b9a1348
Sophie <65089742+sophie-72@users.noreply.github.com>
2025-07-13 21:28:50 -0400

    fix pylint warnings

--- src/models/program.py
+++ src/models/program.py
@@ -1,7 +1,5 @@
 """Analyzes the provided file."""
 
-from builtins import _NotImplementedType
-
 from elftools.elf.elffile import ELFFile
 
 from src.control_flow_graph import print_main_function_graph
@@ -26,10 +24,15 @@ class Program:
             self.__function_symbols = elf_processor.get_function_names()
 
     def analyze(self) -> None:
+        """
+        Analyze the ELF file based on instructions, relocations, strings and
+        function names extracted from the executable.
+        """
         translate_instructions(self.__instructions, self.__relocations, self.__strings)
         write_to_file(self.__executable_name, self.__instructions)
         functions = identify_functions(self.__instructions, self.__function_symbols)
         print_main_function_graph(functions)
 
-    def display_analysis(self) -> _NotImplementedType:
-        return NotImplemented
+    def display_analysis(self) -> None:
+        """Display the analysis results."""
+        raise NotImplementedError
