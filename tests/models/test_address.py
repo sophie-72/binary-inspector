1cdbbf6325c3902070e58e76b6cd88c7309cb812
Sophie <65089742+sophie-72@users.noreply.github.com>
2025-07-20 17:28:01 -0400

    add test_address.py

---
+++ tests/models/test_address.py
@@ -0,0 +1,25 @@
+import unittest
+
+from src.models import Address
+
+ANY_VALUE = 1
+
+
+class TestAddress(unittest.TestCase):
+    def test_same_value(self):
+        first_address = Address(ANY_VALUE)
+        second_address = Address(ANY_VALUE)
+
+        self.assertEqual(first_address, second_address)
+
+    def test_different_value(self):
+        first_address = Address(ANY_VALUE)
+        second_address = Address(ANY_VALUE + 1)
+
+        self.assertNotEqual(first_address, second_address)
+
+    def test_different_objects(self):
+        first_address = Address(ANY_VALUE)
+        second_address = "not an address"
+
+        self.assertNotEqual(first_address, second_address)
